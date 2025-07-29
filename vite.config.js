f7c22c5bad756f569b195af2d818f44d90c7dcc5
Artem <mister_paint03@mail.ru>
2025-07-30 01:59:42 +0300

    bugfix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    allowedHosts: [
+      'admirable-twilight-214a45.netlify.app'
+    ]
+  }
 })
